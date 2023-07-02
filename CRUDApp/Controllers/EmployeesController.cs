edd481db9d7a0446a60b1d70c7514ccbe76b7b40
Himanshi Jain <Himanshi Jain@LAPTOP-0RLLHN1N>
2023-07-02 18:27:51 +0530

    Handled possible exceptions

--- CRUDApp/Controllers/EmployeesController.cs
+++ CRUDApp/Controllers/EmployeesController.cs
@@ -10,35 +10,47 @@ namespace CRUDApp.Controllers
     public class EmployeesController : ControllerBase
     {
         private IDataAccessProvider _dataAccessProvider;
-        private readonly ILogger _logger;
+        private readonly ILogger<EmployeesController> _logger;
 
-        public EmployeesController(IDataAccessProvider dataAccessProvider,ILogger logger)
+        public EmployeesController(IDataAccessProvider dataAccessProvider,ILogger<EmployeesController> logger)
         {
             _dataAccessProvider = dataAccessProvider;
             _logger = logger;
         }
 
         [HttpPost]
-        public async Task<IActionResult> AddEmployeeRecord(EmployeeModel employee)
+        public async Task<IActionResult> AddEmployeeRecord([FromBody] EmployeeModel employee)
         {
-            _logger.LogInformation($"Initiating addition of an employee named {employee.first_name}");
-            int empId = await _dataAccessProvider.AddEmployeeRecordAsync(employee);
-            if (empId != 0)
+            _logger.LogInformation($"{DateTime.Now}: Initiating addition of an employee named {employee.first_name}");
+
+            try
             {
-                _logger.LogInformation($"{employee.first_name} added successfully at ID{empId}");
-                return new ObjectResult(employee) { StatusCode = StatusCodes.Status201Created };
+                if (employee == null)
+                {
+                    throw new ArgumentNullException("Data cannot be null.");
+                }
+                int empAddedSuccess = await _dataAccessProvider.AddEmployeeRecordAsync(employee);
+                if (empAddedSuccess != 0)
+                {
+                    _logger.LogInformation($"{DateTime.Now}: {employee.first_name} added successfully");
+                    return new ObjectResult(employee) { StatusCode = StatusCodes.Status201Created };
+                }
+                else
+                {
+                    _logger.LogError($"{DateTime.Now}: Error while inserting data");
+                    return BadRequest($"Error while inserting data {StatusCodes.Status500InternalServerError}");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                _logger.LogError($"Error while inserting data/Employee already exists");
-                return BadRequest($"Error while inserting data/Employee already exists");
+                return BadRequest(ex.Message);
             }
         }
 
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateEmployeeRecord(int id, EmployeeModel employee)
         {
-            _logger.LogInformation($"Initiating updation of an employee named {employee.first_name}");
+            _logger.LogInformation($"{DateTime.Now}: Initiating updation of an employee named {employee.first_name}");
             ///summary
             ///ModelState.IsValid indicates if it was possible to bind the incoming values 
             ///from the request to the model correctly and whether any explicitly specified 
@@ -47,73 +59,102 @@ namespace CRUDApp.Controllers
             ///
             try
             {
+                if (employee == null)
+                {
+                    throw new ArgumentNullException("Data not found");
+                }    
                 int res = await _dataAccessProvider.UpdateEmployeeRecordAsync(employee, id);
                 if (res != 0)
                 {
-                    _logger.LogInformation($"Data of {employee.first_name} updated successfully");
-                    return new ObjectResult(employee) { StatusCode = StatusCodes.Status204NoContent };
+                    _logger.LogInformation($"{DateTime.Now}: Data of {employee.first_name} updated successfully");
+                    return Ok($"Data of {employee.first_name} updated successfully");
                 }
                 else
                 {
-                    _logger.LogError($"Employee with {id} not found");
+                    _logger.LogError($"{DateTime.Now}: Employee with {id} not found");
                     return NotFound($"Employee with {id} not found");
                 }
             }
             catch (Exception ex)
             {
-                _logger.LogError($"Something went wrong inside UpdateOwner action: {ex.Message}");
-                return StatusCode(500, "Internal server error");
+                _logger.LogError($"{DateTime.Now}: Something went wrong inside Update action: {ex.Message}");
+                return Problem(
+                    title:"Something went wrong inside Update action",
+                    statusCode:StatusCodes.Status400BadRequest
+                );
             }
         }
 
         [HttpGet]
         public async Task<IActionResult> GetAllEmployees()
         {
-            _logger.LogInformation("Retrieving all employees data");
-            List<EmployeeModel> employees = await _dataAccessProvider.GetAllEmployeeRecordAsync();
-            if (employees != null)
+            _logger.LogInformation($"{DateTime.Now}: Retrieving all employees data");
+            try
             {
-                _logger.LogInformation("Data retrieved successfully");
-                return Ok(employees);
+                List<EmployeeModel> employees = await _dataAccessProvider.GetAllEmployeeRecordAsync();
+                if (employees != null)
+                {
+                    _logger.LogInformation($"{DateTime.Now}: Data retrieved successfully");
+                    return Ok(employees);
+                }
+                else
+                {
+                    _logger.LogError($"{DateTime.Now}: Error retrieving the data");
+                    return BadRequest($"Error while retrieving the data");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                _logger.LogError($"Error retrieving the data");
-                return BadRequest($"Error while retrieving the data");
+                return BadRequest($"{StatusCodes.Status500InternalServerError} \n {ex.Message}");
             }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetEmployeeRecordById(int id)
         {
-            _logger.LogInformation($"Retrieving data of an employee ID:{id}");
-            EmployeeModel emp = await _dataAccessProvider.GetEmployeeRecordByIdAsync(id);
-            if (emp != null)
+            _logger.LogInformation($"{DateTime.Now}: Retrieving data of an employee ID:{id}");
+
+            try
             {
-                _logger.LogInformation($"Employee data retrieved");
-                return Ok(emp);
+                EmployeeModel emp = await _dataAccessProvider.GetEmployeeRecordByIdAsync(id);
+                if (emp != null)
+                {
+                    _logger.LogInformation($"{DateTime.Now}: Employee data retrieved");
+                    return Ok(emp);
+                }
+                else
+                {
+                    _logger.LogError($"{DateTime.Now}: Employee not found");
+                    return NotFound($"Employee not found");
+                }
             }
-            else
+            catch (Exception ex)
             {
-                _logger.LogError("Employee not found");
-                return NotFound($"Employee not found");
+                return BadRequest($"{StatusCodes.Status500InternalServerError} \n {ex.Message}");
             }
         }
 
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteEmployeeRecordById(int id)
         {
-            _logger.LogInformation($"Initiating deletion of an employee ID:{id}");
-            int res = await _dataAccessProvider.DeleteEmployeeRecordAsync(id);
-            if ( res == 1)
+            _logger.LogInformation($"{DateTime.Now}: Initiating deletion of an employee ID:{id}");
+            try
             {
-                _logger.LogInformation("Data deleted successfully");
-                return Ok("Data deleted successfully");
+                int res = await _dataAccessProvider.DeleteEmployeeRecordAsync(id);
+                if (res == 1)
+                {
+                    _logger.LogInformation($"{DateTime.Now}: Data deleted successfully");
+                    return Ok("Data deleted successfully");
+                }
+                else
+                {
+                    _logger.LogError($"{DateTime.Now}: Employee not found");
+                    return NotFound("Employee not found");
+                }
             }
-            else
+            catch(Exception ex)
             {
-                _logger.LogError("Employee not found");
-                return NotFound("Employee not found");
+                return BadRequest($"{StatusCodes.Status500InternalServerError} \n {ex.Message}");
             }
         }
     }
---
+++ CRUDApp/Models/UpdateEmployeeModel.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApp.Models
+{
+    public class UpdateEmployeeModel
+    {
+        [Required]
+        [StringLength(50)]
+        public string ?first_name { get; set; }
+       
+        [Required]
+        [StringLength(50)]
+        public string ?last_name { get; set;}
+
+        [Required]
+        [StringLength(50)]
+        public string ?country { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string ?gender { get; set; }
+
+        [Required]
+        public double salary { get; set; }
+    }
+}
